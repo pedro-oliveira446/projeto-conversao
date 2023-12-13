88bf79f14790cafca5e3992e6a1905bc8702e3be
Pedro Leite <pedro.leite@liferay.com>
2023-12-12 23:36:32 -0300

    Caculo secao magnetica e geometrica

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 def divide(a, b):
     return round(a / b,2)
 
@@ -34,10 +36,16 @@ def main():
 
     media_densidades = round( (densidade_corrente_primaria + densidade_corrente_secundaria) / 2 ,2 )
 
+    secao_magnetica_nucleo = round(7.5 * math.sqrt(divide(potencia_secundaria, frequencia)), 2)
+
+    secao_geometrica_nucleo = secao_magnetica_nucleo * 1.1;
+
     print(secao_condutor_primario,corrente_primaria)
     print(secao_condutor_secundario,corrente_secundaria)
     print(densidade_corrente_primaria,densidade_corrente_secundaria)
     print(media_densidades)
+    print(secao_magnetica_nucleo)
+    print(secao_geometrica_nucleo)
 
 if __name__ == "__main__":
     main()
